481444afba20f2e3c38a53806bd186882d31fcb8
Héctor Salazar <h.salazar509@gmail.com>
2016-06-24 00:13:06 -0500

    Julieta is now executable

--- lib/julieta.rb
+++ lib/julieta.rb
@@ -2,14 +2,4 @@ require "julieta/version"
 
 module Julieta
 
-  class Romeo
-    def initialize
-      @name = "Romeo"
-    end
-
-    def find_julieta
-      puts "Are you here?..."
-    end
-  end
-
 end
--- lib/julieta/version.rb
+++ lib/julieta/version.rb
@@ -1,3 +1,12 @@
 module Julieta
   VERSION = "0.0.1a"
+  class Romeo
+    def initialize
+      @name = "Romeo"
+    end
+
+    def find_julieta
+      puts "Are you here?..."
+    end
+  end
 end

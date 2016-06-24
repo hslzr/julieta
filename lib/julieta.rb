16d3fdecac4be6e27dcfb256ce2d11caa0edcbb8
Héctor Salazar <h.salazar509@gmail.com>
2016-06-24 00:06:20 -0500

    class Romeo

--- lib/julieta.rb
+++ lib/julieta.rb
@@ -1,5 +1,15 @@
 require "julieta/version"
 
 module Julieta
-  # Your code goes here...
+
+  class Romeo
+    def initialize
+      @name = "Romeo"
+    end
+
+    def find_julieta
+      puts "Are you here?..."
+    end
+  end
+
 end
